2bc8b04e39e75e32f06e09deafb6f041ed090ed3
kevllino <kevin.eid@gonitro.com>
2017-04-03 23:54:17 +0100

    Add tests to data processing

--- 03-Spark/stackoverflow/src/main/scala/stackoverflow/StackOverflow.scala
+++ 03-Spark/stackoverflow/src/main/scala/stackoverflow/StackOverflow.scala
@@ -8,7 +8,6 @@ import scala.annotation.tailrec
 /** A raw stackoverflow posting, either a question or an answer */
 case class Posting(postingType: Int, id: Int, acceptedAnswer: Option[Int], parentId: Option[Int], score: Int, tags: Option[String]) extends Serializable
 
-
 /** The main class */
 object StackOverflow extends StackOverflow {
 
--- 03-Spark/stackoverflow/src/test/scala/stackoverflow/StackOverflowSuite.scala
+++ 03-Spark/stackoverflow/src/test/scala/stackoverflow/StackOverflowSuite.scala
@@ -7,19 +7,45 @@ import org.scalatest.{BeforeAndAfterAll, FunSuite}
 @RunWith(classOf[JUnitRunner])
 class StackOverflowSuite extends FunSuite with BeforeAndAfterAll {
 
+  def initializeStackOverflow(): Boolean =
+    try {
+      StackOverflow
+      true
+    } catch {
+      case ex: Throwable =>
+        println(ex.getMessage)
+        ex.printStackTrace()
+        false
+    }
 
-  lazy val testObject = new StackOverflow {
-    override val langs =
-      List(
-        "JavaScript", "Java", "PHP", "Python", "C#", "C++", "Ruby", "CSS",
-        "Objective-C", "Perl", "Scala", "Haskell", "MATLAB", "Clojure", "Groovy")
-    override def langSpread = 50000
-    override def kmeansKernels = 45
-    override def kmeansEta: Double = 20.0D
-    override def kmeansMaxIterations = 120
+  override def afterAll(): Unit = {
+    assert(initializeStackOverflow(), " -- did you fill in all the values in StackOverflow (conf, sc)?")
+    import StackOverflow._
+    sc.stop()
   }
 
-  test("testObject can be instantiated") {
+    lazy val testObject = new StackOverflow {
+      override val langs =
+        List(
+          "JavaScript", "Java", "PHP", "Python", "C#", "C++", "Ruby", "CSS",
+          "Objective-C", "Perl", "Scala", "Haskell", "MATLAB", "Clojure", "Groovy")
+
+      override def langSpread = 50000
+
+      override def kmeansKernels = 45
+
+      override def kmeansEta: Double = 20.0D
+
+      override def kmeansMaxIterations = 120
+    }
+
+  val rawPosts = List(
+    Posting(1, 345, None, None, 140, Some("Scala")),
+    Posting(2, 346, None, Some(345), 67, Some("Scala")),
+    Posting(2, 347, None, Some(345), 55, Some("Scala"))
+  )
+
+  test("testObject can be instantiated")  {
     val instantiatable = try {
       testObject
       true
@@ -29,5 +55,31 @@ class StackOverflowSuite extends FunSuite with BeforeAndAfterAll {
     assert(instantiatable, "Can't instantiate a StackOverflow object")
   }
 
+  test("test groupedPostings") {
+    assert(initializeStackOverflow(), " -- did you fill in all the values in StackOverflow (conf, sc)?")
+    import StackOverflow._
+    val rdd = sc.parallelize(rawPosts)
+    val groupped = groupedPostings(rdd).map(_._2).flatMap(el => el.map(_._2))
+    assert(groupped.count() === 2)
+  }
+
+  test("test scoredPostings") {
+    assert(initializeStackOverflow(), " -- did you fill in all the values in StackOverflow (conf, sc)?")
+    import StackOverflow._
+    val rdd = sc.parallelize(rawPosts)
+    val groupped = groupedPostings(rdd)
+    val scored = scoredPostings(groupped)
+    assert(scored.map(_._2).collect()(0) === 67)
+  }
+
+  test("test vectorPostings") {
+    assert(initializeStackOverflow(), " -- did you fill in all the values in StackOverflow (conf, sc)?")
+    import StackOverflow._
+    val rdd = sc.parallelize(rawPosts)
+    val groupped = groupedPostings(rdd)
+    val scored = scoredPostings(groupped)
+    val vectored = vectorPostings(scored)
+    assert(vectored.collect() === Array((10, 67)))
+  }
 
 }
