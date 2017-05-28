8e19aef30091b4db9365a30ee1e1314269372df6
kevllino <kevin.eid@gonitro.com>
2017-05-28 21:44:55 +0100

    Fix interpolateRGB forgot to round

--- observatory/src/main/scala/observatory/models.scala
+++ observatory/src/main/scala/observatory/models.scala
@@ -22,7 +22,7 @@ object Color {
     (-60.0,	Color(0,0,0)))
 
   def interpolateRGB(t0: Double, c0: Int, t1: Double, c1: Int, temperature: Double): Int = {
-    (c0 + (temperature - t0) / (t1 - t0) * (c1 - c0)).toInt
+   round((c0 + (temperature - t0) / (t1 - t0) * (c1 - c0))).toInt
   }
 }
 
--- observatory/src/test/scala/observatory/VisualizationTest.scala
+++ observatory/src/test/scala/observatory/VisualizationTest.scala
@@ -32,12 +32,52 @@ class VisualizationTest extends FunSuite with Checkers {
 
   }
 
-  ignore("Predict temperature") {
-   val predictedTemp = predictTemperature(averageTempPerLocation, someWhereInRussia)
-    println(locatedTemperatures)
-    println(averageTempPerLocation)
-    println(s"Pred temp is $predictedTemp")
+  ignore("Predicting temperature at a known location") {
+    val knownLocation = Location(61.383,005.867)
+    val predictedTemp = predictTemperature(averageTempPerLocation, knownLocation)
 
+    assert(predictedTemp === 40.55555555555556)
+
+  }
+
+  ignore("predictTemperature works with 1975"){
+    val year = 1975
+    val stationsFile = "/stations.csv"
+    val temperaturesFile = "/1975.csv"
+
+    val lt = locateTemperatures(year, stationsFile, temperaturesFile)
+    assert(lt.size === 2177190)
+    val lya = locationYearlyAverageRecords(lt)
+    assert(lya.size === 8253)
+
+    val arrayOfLatLon = (for {
+      lat <- 90 to -89 by -1
+      lon <- -180 to 179
+    } yield (lat, lon)).toArray
+
+    val arrayOfTemps = arrayOfLatLon.par.map{case (lat, lon) => predictTemperature(lya, Location(lat, lon))}
+    assert(arrayOfTemps.size === arrayOfLatLon.size)
+  }
+
+
+  test("interpolateColor() should work with some records"){
+    val cs = Seq(
+      (200.0, Color(100, 200, 300)),
+      (100.0, Color(100, 100, 100))
+    ).toIterable
+
+    assert(Color.interpolateRGB(100.0, 100, 200.0, 200, 150.0) === 150)
+    assert(Color.interpolateRGB(100.0, 100, 200.0, 200, 100.0) === 100)
+
+    assert(interpolateColor(cs, 250.0) === Color(100, 200, 300))
+    assert(interpolateColor(cs, 50.0) === Color(100, 100, 100))
+
+    assert(interpolateColor(Color.scale, 60.0) === Color(255, 255, 255))
+    assert(interpolateColor(Color.scale, 32.0) === Color(255, 0, 0))
+    assert(interpolateColor(Color.scale, 12.0) === Color(255, 255, 0))
+
+    assert(interpolateColor(Color.scale, 46.0) === Color(255, 128, 128))
+    assert(interpolateColor(Color.scale, 22.0) === Color(255, 128, 0))
   }
 
 }
